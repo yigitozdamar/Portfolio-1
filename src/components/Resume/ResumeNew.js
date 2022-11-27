65f7683a5a8b9d648d54b62e6eefa30078fcd7c8
yigit <yigitozdamar@hotmail.com>
2022-11-27 21:00:16 +0300

    ..

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/YigitOZDAMAR_Resume.pdf";
+import pdf from "../../Assets/../Assets/YigitOZDAMAR_CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/yigitozdamar/Portfolio-1/master/src/Assets/YigitOZDAMAR_Resume.pdf";
+  "https://raw.githubusercontent.com/yigitozdamar/Portfolio-1/master/src/Assets/YigitOZDAMAR_CV.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
