e89275473c87bbce0c71260000cbef56a369247a
yigit <yigitozdamar@hotmail.com>
2023-06-05 15:25:38 +0300

    mail added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Developed by Yigit Ozdamar</h3>
+          <h3>Developed by Yigit Ozdamar yigitozdamar129@gmail.com</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} YO</h3>
